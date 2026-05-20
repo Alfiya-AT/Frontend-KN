b40117a2b20d77e8fe411c329e1a0632da61fae8
Alfiya-AT <alfiyat391@gmail.com>
2026-05-20 14:16:04 +0530

    css animation

--- JS/operators.js
+++ JS/operators.js
@@ -141,99 +141,112 @@
 // console.log([1,2,3] instanceof Array); // true
 // console.log([1,2] instanceof Object); // true
 
+// ====================14/5/26====================
 
+console.log(5+10); // 15
+console.log("5"+10); // "510" - string concatenation
+console.log(5+"10");
+
+
+
+
+
+
+
+
+// =======================18/5/26========================
 // int+srt=str
-console.log(1+"2"+3);//123
-console.log(1+2+"3");//33
-// due to - 3 will behave like int 
-console.log("3"-1);//2
-// str + int = str
-console.log("3"+1);//31
+// console.log(1+"2"+3);//123
+// console.log(1+2+"3");//33
+// // due to - 3 will behave like int 
+// console.log("3"-1);//2
+// // str + int = str
+// console.log("3"+1);//31
 
-console.log("3"*"1");//3
+// console.log("3"*"1");//3
 
 
-console.log(0==false); // true 
-console.log(0===false); // false - data type is different
+// console.log(0==false); // true 
+// console.log(0===false); // false - data type is different
 
-console.log(null == undefined); // true
-console.log(null === undefined); // false - data type is different
+// console.log(null == undefined); // true
+// console.log(null === undefined); // false - data type is different
 
-console.log(NaN == NaN); // true
-console.log(NaN === false); // false - data type is different
+// console.log(NaN == NaN); // true
+// console.log(NaN === false); // false - data type is different
 
-let a=5
-console.log(a++); // 5
-console.log(a); // 6
+// let a=5
+// console.log(a++); // 5
+// console.log(a); // 6
 
-let b=5
-console.log(++b); // 6
+// let b=5
+// console.log(++b); // 6
 
-let x=5
-let y=x++ + ++x;
-console.log(y); // 5 + 7 = 12
+// let x=5
+// let y=x++ + ++x;
+// console.log(y); // 5 + 7 = 12
 
-console.log(true + true);//2
-console.log(true + false);//1
-console.log(true - false);//1
-console.log(true * false);//0
-console.log(true / false);//Infinity
+// console.log(true + true);//2
+// console.log(true + false);//1
+// console.log(true - false);//1
+// console.log(true * false);//0
+// console.log(true / false);//Infinity
 
-console.log([] == false);//true
-console.log(5 && 10);//10
-console.log(0 || 100);//100
-console.log(!"hello");//false
-console.log(!0);//true
-console.log([] == ![]); // true
+// console.log([] == false);//true
+// console.log(5 && 10);//10
+// console.log(0 || 100);//100
+// console.log(!"hello");//false
+// console.log(!0);//true
+// console.log([] == ![]); // true
 
-console.log("10"*"2");
-console.log("10"/2);
-console.log("hello"*2);//NaN Not A Number
-console.log(typeof NaN); // number
-console.log(typeof null); // object
-console.log([]+[]);//""
-console.log([]+{});//"[object Object]"
-console.log({}+[]);//0
+// console.log("10"*"2");
+// console.log("10"/2);
+// console.log("hello"*2);//NaN Not A Number
+// console.log(typeof NaN); // number
+// console.log(typeof null); // object
+// console.log([]+[]);//""
+// console.log([]+{});//"[object Object]"
+// console.log({}+[]);//0
 
-// {}=> Object
-let c="5";
-let d=2;
-console.log(c-d+c);//35
-console.log([]==![]);// true
-console.log([]==0); // true 
-console.log([1,2]+[3,4]);// "1,2,3,4"
-console.log("2">"12");// true - lexicographical comparison
-console.log(2>"12");// false - numeric comparison
-console.log(Boolean("false"));// true
-console.log(Boolean("")); // false
-console.log(c==d); // true
-console.log(c===d); // false
+// // {}=> Object
+// let c="5";
+// let d=2;
+// console.log(c-d+c);//35
+// console.log([]==![]);// true
+// console.log([]==0); // true 
+// console.log([1,2]+[3,4]);// "1,2,3,4"
+// console.log("2">"12");// true - lexicographical comparison
+// console.log(2>"12");// false - numeric comparison
+// console.log(Boolean("false"));// true
+// console.log(Boolean("")); // false
+// console.log(c==d); // true
+// console.log(c===d); // false
 
-console.log(100/"10");//10
-console.log(true == "1");//true
-console.log(true === "0");//true
-console.log(false == "0");//false
+// console.log(100/"10");//10
+// console.log(true == "1");//true
+// console.log(true === "0");//true
+// console.log(false == "0");//false
 
-console.log(1<2<3);//true
-console.log(3>2>1);//false
+// console.log(1<2<3);//true
+// console.log(3>2>1);//false
 
-console.log(+"5");// 5
-console.log(+"hello"); // NaN
+// console.log(+"5");// 5
+// console.log(+"hello"); // NaN
 
-let p=10;
-console.log(p+=5);// p = p + 5 => 15
+// let p=10;
+// console.log(p+=5);// p = p + 5 => 15
 
-let q=10;
-console.log(q*=5);// q = q * 5 => 50
+// let q=10;
+// console.log(q*=5);// q = q * 5 => 50
 
-console.log(5**3); // 125
+// console.log(5**3); // 125
 
-console.log(typeof typeof 1);// string : "number" => "string"
-console.log([] instanceof Array); // true
-console.log([] instanceof Object); // true
+// console.log(typeof typeof 1);// string : "number" => "string"
+// console.log([] instanceof Array); // true
+// console.log([] instanceof Object); // true
 
-console.log([] == ""); // true
-console.log([0] == 0); // false
+// console.log([] == ""); // true
+// console.log([0] == 0); // false
 
-console.log([1] == 1); // true
-console.log([1] === 1); // false
+// console.log([1] == 1); // true
+// console.log([1] === 1); // false
